ea5eed3e6ca0848a3423dc5f1ee3671bab6366e8
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-20 21:25:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,18 @@
 #include<fstream>
 #include<locale>
 #include"textgen.h"
-int main()
-{
-	const int NPREF = 2;
-	const int MAXGEN = 1000;
-	const int seed = 100;
-	setlocale(LC_ALL, "rus");
-	std::ifstream file("text.txt");
-	std::vector<std::string>words;
-	std::string word;
-	while (file>>word) {
-		words.push_back(word);
-	}
-	MarkovChain markov;
-	markov.Tab(words, NPREF);
-	std::cout<<markov.CreateText(MAXGEN, seed);
+int main(){
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    const int seed = 100;
+    setlocale(LC_ALL, "rus");
+    std::ifstream file("text.txt");
+    std::vector<std::string>words;
+    std::string word;
+    while (file >> word) {
+        words.push_back(word);
+    }
+    MarkovChain markov;
+    markov.Tab(words, NPREF);
+    std::cout << markov.CreateText(MAXGEN, seed);
 }
