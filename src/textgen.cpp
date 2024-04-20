1eef36d22d5f4ff69de816ea8d399a79aeeb37c2
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-20 21:11:59 +0300

    Create textgen.cpp

---
+++ src/textgen.cpp
@@ -0,0 +1,37 @@
+// Copyright 2022 UNN-IASR
+#include "MarkovChain.h"
+void MarkovChain::Tab(std::vector<std::string> words,int NREF) {
+	prefix pref;
+	int global = 0;
+	for (int i = 0; i < NREF; i++) {
+		pref.push_back(words[global]);
+		global++;
+	}
+	fpref = pref;
+	while (global < words.size()) {
+		statetab[pref].push_back(words[global]);
+		pref.pop_front();
+		pref.push_back(words[global]);
+		global++;
+	}
+}
+std::string MarkovChain::CreateText(int MAXGEN, unsigned int seed) {
+	srand(seed);
+	int randomValue;
+	std::string textOut;
+	std::string suffix;
+	prefix pref = fpref;
+	for (int i = 0; i < fpref.size(); i++) {
+		textOut += fpref[i] + " ";
+		MAXGEN--;
+	}
+	for (int i = 0; i < MAXGEN; i++) {
+		if (statetab.find(pref) == statetab.end()) break;
+		randomValue= rand() % statetab[pref].size();
+		suffix = statetab[pref][randomValue];
+		textOut += suffix + " ";
+		pref.pop_front();
+		pref.push_back(suffix);
+	}
+	return textOut;
+}
