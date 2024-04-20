2c8fb43648001e30ef154f7e9f3285573ff07a0c
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-20 21:21:32 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,16 +1,15 @@
 // Copyright 2022 UNN-IASR
 #pragma once
+#include<stdlib.h>
 #include<string>
 #include<map>
 #include<deque>
-#include<stdlib.h>
 #include<vector>
-class MarkovChain
-{
+class MarkovChain{
 public:
-	typedef std::deque<std::string> prefix;
-	std::map<prefix, std::vector<std::string> > statetab;
-	void Tab(std::vector<std::string> words, int NREF);
-	std::string CreateText(int MAXGEN, unsigned int seed);
-	prefix fpref;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > statetab;
+    void Tab(std::vector<std::string> words, int NREF);
+    std::string CreateText(int MAXGEN, unsigned int seed);
+    prefix fpref;
 };
