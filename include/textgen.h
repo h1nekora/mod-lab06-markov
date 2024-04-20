6117c83e28b2045451dea47a129a5c0a455053c5
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-20 21:10:07 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,16 @@
+// Copyright 2022 UNN-IASR
+#pragma once
+#include<string>
+#include<map>
+#include<deque>
+#include<stdlib.h>
+#include<vector>
+class MarkovChain
+{
+public:
+	typedef std::deque<std::string> prefix;
+	std::map<prefix, std::vector<std::string> > statetab;
+	void Tab(std::vector<std::string> words, int NREF);
+	std::string CreateText(int MAXGEN, unsigned int seed);
+	prefix fpref;
+};
