e6ca489cac02d76443e38cb867797b1ba3e025fc
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-21 00:35:40 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -34,7 +34,7 @@ TEST(test4, ChooseSuffixMnogo) {
     "2", "3", "1", "2", "4", "5", "1"};
     MarkovChain mark;
     mark.Tab(words, 1);
-    EXPECT_EQ(mark.CreateText(10, 10), "1 2 4 5 2 4 5 1 2 3");
+    EXPECT_EQ(mark.CreateText(10, 10), "1 2 4 5 2 4 5 1 2 3 ");
 }
 
 TEST(test5, FormText) {
