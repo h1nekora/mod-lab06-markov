83084f1c7530324cef4a09a6a2ea899ea0920780
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-20 21:10:52 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+// Copyright 2022 UNN-IASR
+#include <iostream>
+#include<fstream>
+#include<locale>
+#include"MarkovChain.h"
+int main()
+{
+	const int NPREF = 2;
+	const int MAXGEN = 1000;
+	const int seed = 100;
+	setlocale(LC_ALL, "rus");
+	std::ifstream file("text.txt");
+	std::vector<std::string>words;
+	std::string word;
+	while (file>>word) {
+		words.push_back(word);
+	}
+	MarkovChain markov;
+	markov.Tab(words, NPREF);
+	std::cout<<markov.CreateText(MAXGEN, seed);
+}
